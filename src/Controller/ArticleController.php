b88e56db1e15e6fd48903795207fb688bca7361d
ahmtysn <ahmtysn14@gmail.com>
2021-03-14 21:52:20 +0100

    create and show article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
   /**
-   * @Route("/",name="home")
+   * @Route("/",name="article_list")
    */
   public function index(): Response
   {
-    $articles = ['article 1', 'article 2'];
-    return $this->render('article/index.html.twig', [
+    // $articles = ['article 1', 'article 2'];
+    $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+    return $this->render('articles/index.html.twig', [
       'articles' => $articles
     ]);
+  }
+
+  /**
+   * @Route("/article/{id}",name="article_show")
+   */
+  public function show($id)
+  {
+    $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+    return $this->render('articles/show.html.twig', [
+      'article' => $article
+    ]);
+  }
+
+  /**
+   * @Route("/article/save",name="save")
+   */
+  public function save()
+  {
+    $em = $this->getDoctrine()->getManager();
+    $article = new Article();
+    $article
+      ->setTitle('Article Two')
+      ->setBody('This is body of article Two!');
+
+    $em->persist($article);
+    $em->flush();
+
+    return new Response("Saves an article with the id of {$article->getId()}");
   }
 }

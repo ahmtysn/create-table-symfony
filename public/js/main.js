537988e11f0a3fd2c269019d9bc2de2e8a45f667
ahmtysn <ahmtysn14@gmail.com>
2021-03-15 13:47:40 +0100

    main.js file added

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+const articles = document.getElementById('articles');
+if (articles) {
+  articles.addEventListener('click', e => {
+    console.log(e.target.className);
+    if (e.target.className === 'btn btn-danger delete-article') {
+      if (confirm('Are you sure?')) {
+        const id = e.target.getAttribute('data-id');
+        fetch(
+          `http://localhost/tuts/symfony-test/public/index.php/article/delete/${id}`,
+        ).then(res => window.location.reload());
+      }
+    }
+  });
+}
